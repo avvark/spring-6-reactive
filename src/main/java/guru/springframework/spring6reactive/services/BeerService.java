90a74d52ea594089094148dd2684daed3db23cc6
Alexander van Vark <a.van.vark@anva.nl>
2023-09-29 07:38:43 +0200

    commit

--- src/main/java/guru/springframework/spring6reactive/services/BeerService.java
+++ src/main/java/guru/springframework/spring6reactive/services/BeerService.java
@@ -1,11 +1,13 @@
 package guru.springframework.spring6reactive.services;
 
 import guru.springframework.spring6reactive.model.BeerDto;
-import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 
 public interface BeerService {
 
   Flux<BeerDto> listBeers();
+
+  Mono<BeerDto> getBeerById(Integer beerId);
 }
